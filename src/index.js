f40610d3d23e0f7c25d321997957f549ffaa57d4
WIN-JR9EEP9V1OJ\medeiva <anastasia.pryakhina@breffi.ru>
2020-10-03 00:04:59 +0200

    refactor: min amd max

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ exports.min = function min (array) {
   if (array === undefined || array.length == 0) {
     return 0;
   } else {
-   return Math.min.apply(null, array);
+   return Math.min(...array);
   }
 }
 
@@ -11,7 +11,7 @@ exports.max = function max (array) {
   if (array === undefined || array.length == 0) {
     return 0;
   } else {
-   return Math.max.apply( null, array );
+   return Math.max( ...array );
   }
 }
 
